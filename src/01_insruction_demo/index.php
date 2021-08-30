6d5ece086f5d358d2569f57bc2d4e1b860b81aa7
jeremy <1280442096@qq.com>
2021-08-30 16:59:30 +0800

    add php instruction demo

---
+++ src/01_insruction_demo/index.php
@@ -0,0 +1,18 @@
+<?php 
+  // 测试 PHP Demo
+  echo "Test demo of PHP!<br>";
+  /*
+    This is a test demo of PHP
+    Auther: Jeremy Wu
+    Date: 2021-08-30
+  */
+  ECHO "Hello World!<br>";
+  echo "Hello World!<br>";
+  EcHo "Hello World!<br>";
+
+  $color="red";
+  echo "My car is " . $color . "<br>";
+  echo "My house is " . $COLOR . "<br>";
+  echo "My boat is " . $coLOR . "<br>";
+
+?>
