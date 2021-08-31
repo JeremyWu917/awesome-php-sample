58512e9e02009691338936318d23c26312dcf070
jeremy <1280442096@qq.com>
2021-08-31 10:00:30 +0800

    add php data type demo

---
+++ src/04_php_data_type_demo/index.php
@@ -0,0 +1,29 @@
+<?php 
+  $x = 5985;
+  var_dump($x);
+  echo "<br>"; 
+  $x = -345; // 负数
+  var_dump($x);
+  echo "<br>"; 
+  $x = 0x8C; // 十六进制数
+  var_dump($x);
+  echo "<br>";
+  $x = 047; // 八进制数
+  var_dump($x);
+
+  $x = 10.365;
+  var_dump($x);
+  echo "<br>"; 
+  $x = 2.4e3;
+  var_dump($x);
+  echo "<br>"; 
+  $x = 8E-5;
+  var_dump($x);
+
+  $cars=array("Volvo","BMW","SAAB");
+  var_dump($cars);
+
+  $x="Hello world!";
+  $x=null;
+  var_dump($x);
+?>
