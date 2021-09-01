5a12c650cfee4454f7d806e32c5709b1ca1d3e93
jeremy <1280442096@qq.com>
2021-09-01 13:54:45 +0800

    add php form handle demo

---
+++ src/16_php_form_handle_demo/welcome_get.php
@@ -0,0 +1,8 @@
+<html>
+  <body>
+    <h2>Get methods</h2>
+    Glad to see you my dear friend <?php
+    echo $_GET["name1"] . " and your email address is : " . $_GET["email1"];
+    ?>
+  </body>
+</html>
---
+++ src/16_php_form_handle_demo/welcome_post.php
@@ -0,0 +1,8 @@
+<html>
+  <body>
+  <h2>Post methods</h2>
+    Glad to see you my dear friend <?php
+    echo $_POST["name"] . " and your email address is : " . $_POST["email"];
+    ?>
+  </body>
+</html>
